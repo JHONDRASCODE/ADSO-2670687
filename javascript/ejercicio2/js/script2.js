8a44b55d119ac6dd10b85a462ce22990f1a35d9c
sebastianramirez <sebastian_ramirezro@soy.sena.edu.co>
2023-07-25 21:55:10 -0500

    se agrego la carpeta a repositorio

---
+++ javascript/ejercicio2/js/script2.js
@@ -0,0 +1,23 @@
+/*EJERCICIO 2- dado el sueldo de un empleado
+imprimir en una alerta en nuevo sueldo 
+del emplado si tuvo un aumento del 10% 
+
+let sueldo= prompt("ingrese el valor del sueldo");
+let psueldo= 10/100;
+let nsueldo= parseInt (sueldo) + parseInt(sueldo*psueldo);
+alert("el nuevo sueldo con el aumento es "+nsueldo);*/
+
+/*Un vendedor que tiene su sueldo vase, resibe una comision de 30% del total de las ventas del mes,
+el quiere saber cuanto ganara en un mes que tuvo 3 ventas */
+ 
+/*let sueldo = prompt("infrese el valor del sueldo");
+let vent1 = prompt("valor de venta");
+let vent2 = prompt("valor de venta");
+let vent3 = prompt("valor de venta");
+let toven = parseInt (vent1) + parseInt(vent2) + parseInt(vent3);
+let pcom = 30/100 
+let com = parseInt(pcom+com);
+let mes = parseInt(com) + parseInt(sueldo);
+alert ("gano en el mes: "+mes )*/
+
+
